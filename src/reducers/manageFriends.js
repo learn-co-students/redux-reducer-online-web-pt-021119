0715b10c14ebb3b3c89eb00b3735e69d5a55be64
cyantis <cyantis@gmail.com>
2019-11-09 15:47:12 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,10 @@
 export function manageFriends(state, action){
+  switch(action.type){
+    case 'ADD_FRIEND':
+      return { name: state.name hometown: state.hometown id: state.id}
+    case 'REMOVE_FRIEND':
+      return {}
+    default:
+    return state
+  }
 }
